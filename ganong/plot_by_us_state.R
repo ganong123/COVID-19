c02151e554cd4fad74621712e3677005c4df6803
Peter Ganong <ganong@gmail.com>
2020-03-26 10:45:52 -0500

    improve names and documentation

--- ganong/plot_by_us_state.R
+++ ganong/plot_by_us_state.R
@@ -11,9 +11,9 @@ read_day <- function(date, state, var_name_) {
 }
 
 read_month <- function(state) {
+  #this function extracts data from March 10 forward
+  #Why? assembling state-level data for days before requires further data cleaning
   bind_rows(
-    #note: days before March 10th do not to have state-level data
-    #without additional cleaning first
     seq(10, 21) %>% map_dfr(read_day, state, `Province/State`),
     seq(22, 25) %>% map_dfr(read_day, state, Province_State)
   ) %>%
@@ -30,5 +30,5 @@ df <- c("New York", "Illinois", "Washington", "Florida") %>%
   scale_y_log10(breaks = c(10, 30, 100, 300, 1000, 3000, 10000, 30000)) +
   scale_x_date(date_breaks = "3 days",
                date_labels = "%b %d")) %>%
-  ggsave("ganong/state_level_plot.png", .)
+  ggsave("ganong/2020_03_25_state_level_plot.png", .)
 
